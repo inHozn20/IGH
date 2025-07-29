3d7af6132d7e738f8e2580ba91134dc24d4d7eac
inHozn20 <141492984+inHozn20@users.noreply.github.com>
2025-07-29 09:25:56 +0900

    v1.0.2 기초 예제 코드 업로드

---
+++ Enemy.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // Image 자료형을 위해 필요
+
+public class Enemy : MonoBehaviour
+{
+    public string enemyName;
+    public int maxHp;
+    public int nowHp;
+    public int atkDmg;
+    public int atkSpeed;
+
+    public GameObject prfHpBar;
+    public GameObject canvas;
+
+    public float height = 1.7f;
+
+    private RectTransform hpBar;
+    private Image nowHpbar;
+
+    private void SetEnemyStatus(string _enemyName, int _maxHp, int _atkDmg, int _atkSpeed)
+    {
+        enemyName = _enemyName;
+        maxHp = _maxHp;
+        nowHp = _maxHp;
+        atkDmg = _atkDmg;
+        atkSpeed = _atkSpeed;
+    }
+
+    public Move sword_man;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (sword_man.attacked)
+            {
+                nowHp -= sword_man.atkDmg;
+                Debug.Log(nowHp);
+                sword_man.attacked = false;
+                if (nowHp <= 0) // 적 사망
+                {
+                    Destroy(gameObject);
+                    Destroy(hpBar.gameObject);
+                }
+            }
+        }
+    }
+
+    void Start()
+    {
+        hpBar = Instantiate(prfHpBar, canvas.transform).GetComponent<RectTransform>();
+
+        if (name.Equals("Enemy1"))
+        {
+            SetEnemyStatus("Enemy1", 100, 10, 1);
+        }
+
+        nowHpbar = hpBar.transform.GetChild(0).GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        Vector3 _hpBarPos = Camera.main.WorldToScreenPoint(
+            new Vector3(transform.position.x, transform.position.y + height, 0));
+        hpBar.position = _hpBarPos;
+
+        nowHpbar.fillAmount = (float)nowHp / (float)maxHp;
+    }
+}
---
+++ Move.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Move : MonoBehaviour
+{
+
+    public float speed = 2f; // 이동 속도
+    private Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    public int maxHp;
+    public int nowHp;
+    public int atkDmg;
+    public float atkSpeed = 1;
+    public bool attacked = false;
+    public Image nowHpbar;
+
+    void AttackTrue()
+    {
+        attacked = true;
+    }
+    void AttackFalse()
+    {
+        attacked = false;
+    }
+
+    void SetAttackSpeed(float speed)
+    {
+        animator.SetFloat("attackSpeed", speed);
+        atkSpeed = speed;
+    }
+
+    void Start()
+    {
+
+
+        //data정의
+
+        maxHp = 50;
+        nowHp = 50;
+        atkDmg = 10;
+
+        transform.position = Vector3.zero;
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        SetAttackSpeed(1.5f);
+    }
+
+    void Update()
+    {
+        /**
+        float h = Input.GetAxisRaw("Horizontal"); // ←: -1, →: 1
+
+        if (h > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1); // 오른쪽
+            animator.SetBool("moving", true);
+        }
+        else if (h < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);  // 왼쪽
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+
+        transform.Translate(new Vector3(h, 0, 0) * speed * Time.deltaTime);
+        
+
+        if (Input.GetKeyDown(KeyCode.A) && !animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            animator.SetTrigger("attack");
+        }
+        **/
+
+        nowHpbar.fillAmount = (float)nowHp / maxHp;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localScale = new Vector3(-1, 1, 1); // 오른쪽
+            animator.SetBool("moving", true);
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localScale = new Vector3(1, 1, 1); // 왼쪽
+            animator.SetBool("moving", true);
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) && !animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            animator.SetTrigger("attack");
+        }
+    }
+
+}
